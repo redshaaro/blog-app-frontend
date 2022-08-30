2452854e3e4f9c612904f01b259843815fcf9a5d
Zeyad Shaarawy <91687957+redshaaro@users.noreply.github.com>
2022-08-30 23:23:29 +0200

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
       e.preventDefault();
       dispatch({ type: "LOGIN_START" });
       try {
-        const res = await axios.post("/auth/login", {
+        const res = await axios.post("https://simple-node-server-niru.herokuapp.com/auth/login", {
           username: userRef.current.value,
           password: passwordRef.current.value,
         });
