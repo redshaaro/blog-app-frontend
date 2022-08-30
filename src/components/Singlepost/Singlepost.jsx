2297662573b5e4e7b5acff1ec2651b288849dde5
Zeyad Shaarawy <91687957+redshaaro@users.noreply.github.com>
2022-08-30 23:25:13 +0200

    Update Singlepost.jsx

--- src/components/Singlepost/Singlepost.jsx
+++ src/components/Singlepost/Singlepost.jsx
@@ -17,7 +17,7 @@ export default function SinglePost() {
 
   useEffect(() => {
     const getPost = async () => {
-      const res = await axios.get("/posts/" + path);
+      const res = await axios.get("https://simple-node-server-niru.herokuapp.com/posts/" + path);
       setPost(res.data);
       setTitle(res.data.title);
       setDesc(res.data.desc);
